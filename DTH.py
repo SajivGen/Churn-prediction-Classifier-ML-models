4345c5c6333c1266b65171c586094cac69335bb1
Sajiv Soman <166986523+SajivGen@users.noreply.github.com>
2024-05-28 14:29:50 +0530

    Update DTH.py

--- DTH.py
+++ DTH.py
@@ -3,7 +3,7 @@ import numpy as np
 import pickle
 
 
-loaded_model = pickle.load(open('DecisionTree.pkl', 'rb'))
+loaded_model = pickle.load(open('SajivGen/Churn-prediction-Classifier-ML-models/blob/main/Classification_models.ipynb/DecisionTree.pkl', 'rb'))
 
 
 def churn_prediction(input_data):
